52ccaa780a43ef6ce8e7f21a06862d1a1bd3b8c7
Daniel Jackson <carpman@mach5>
2015-06-13 14:23:04 -0500

    Executes commands and replies with their output

--- shove.c
+++ shove.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wordexp.h>
+#include <string.h>
 
 #include <strophe.h>
 #include <confuse.h>
@@ -12,10 +13,18 @@ static cfg_opt_t connection_opts[] = {
     CFG_END()
 };
 
-static cfg_opt_t config_opts[] = {
-    CFG_SEC("connection", connection_opts, CFGF_NONE),
+static cfg_opt_t command_opts[] = {
+    CFG_STR("execute", "", CFGF_NONE),
     CFG_END()
 };
+
+static cfg_opt_t config_opts[] = {
+    CFG_SEC("connection", connection_opts, CFGF_NONE),
+    CFG_SEC("command", command_opts, CFGF_MULTI | CFGF_TITLE),
+    CFG_END()
+};
+
+static cfg_t *cfg = NULL;
 
 int message_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza,
                     void * const userdata){
@@ -29,7 +38,55 @@ int message_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza,
 
     intext = xmpp_stanza_get_text(xmpp_stanza_get_child_by_name(stanza, "body"));
 
-    printf("Incoming message: %s\n",intext);
+    int n = cfg_size(cfg, "command");
+    int found = 0;
+    int i = 0;
+    for(i = 0; i < n; i++){
+        cfg_t *command_config = cfg_getnsec(cfg, "command", i);
+        if(!strncmp(cfg_title(command_config), intext, strlen(cfg_title(command_config)))){
+            found = 1;
+            FILE *processio = popen(cfg_getstr(command_config, "execute"), "r");
+
+            if(processio > 0){
+                char buffer[128];
+                char *result_buffer = NULL;
+
+                while(fgets(buffer, sizeof(buffer), processio) != NULL){
+                    if(result_buffer){
+                        char *new_result_buffer = malloc(strlen(result_buffer)+sizeof(buffer)+1);
+                        strcat(new_result_buffer, result_buffer);
+                        strcat(new_result_buffer, buffer);
+                        free(result_buffer);
+                    }else{
+                        result_buffer = malloc(sizeof(buffer)+1);
+                        strcpy(result_buffer, buffer);
+                    }
+                }
+
+                int result = pclose(processio);
+
+                reply = xmpp_stanza_new(ctx);
+                xmpp_stanza_set_name(reply, "message");
+                xmpp_stanza_set_type(reply, xmpp_stanza_get_type(stanza)?xmpp_stanza_get_type(stanza):"chat");
+                xmpp_stanza_set_attribute(reply, "to", xmpp_stanza_get_attribute(stanza, "from"));
+
+                body = xmpp_stanza_new(ctx);
+                xmpp_stanza_set_name(body, "body");
+
+                text = xmpp_stanza_new(ctx);
+                sprintf(buffer, result_buffer);
+                xmpp_stanza_set_text(text, buffer);
+
+                xmpp_stanza_add_child(body, text);
+                xmpp_stanza_add_child(reply, body);
+
+                xmpp_send(conn, reply);
+                xmpp_stanza_release(reply);
+                free(result_buffer);
+            }
+            break;
+        }
+    }
 
     return 1;
 }
@@ -58,7 +115,6 @@ int main(int argc, char **argv){
     xmpp_log_t *log;
     char *jid, *pass;
     wordexp_t exp_result;
-    cfg_t *cfg;
 
     cfg = cfg_init(config_opts, CFGF_NOCASE);
 
